16235e97beb3d5209ee849d75e721912368d140a
Mendel Feygelson <mfeygelson@gmail.com>
2015-02-11 17:16:40 -0600

    actually move taking remainder to constructor

--- PolyRings.jl
+++ PolyRings.jl
@@ -13,27 +13,28 @@ function X_mod{T<:Number}(m::Polynomial{T})
   eval(quote
          immutable $Ring <: PolyRing{$T}
            v :: Polynomial{$T}
+           $Ring(v::Polynomial{$T}) = new(v % $m)
          end
 
          Base.promote_rule(::Type{$T}, ::Type{$Ring}) = $Ring
-         Base.convert(::Type{$Ring}, x::$T) = $Ring(x)
+         Base.convert(::Type{$Ring}, x::$T) = $Ring(Polynomial(x))
 
          modulus(::Type{$Ring}) = $m
          modulus(::$Ring) = $m
 
          function Base.convert(::Type{$Ring}, x::PolyRing{$T})
            if modulus(x) % $m == 0
-             $Ring(x.v % $m)
+             $Ring(x.v)
            else
              error("Incompatible moduli")
            end
          end
 
-         +(a::$Ring, b::$Ring) = $Ring((a.v+b.v)%$m)
-         -(a::$Ring, b::$Ring) = $Ring((a.v-b.v)%$m)
-         -(a::$Ring) = $Ring((-a.v)%$m)
-         *(a::$Ring, b::$Ring) = $Ring((a.v*b.v)%$m)
-         ==(a::$Ring, b::$Ring) = (a.v-b.v)%$m == 0
+         +(a::$Ring, b::$Ring) = $Ring(a.v+b.v)
+         -(a::$Ring, b::$Ring) = $Ring(a.v-b.v)
+         -(a::$Ring) = $Ring(-a.v)
+         *(a::$Ring, b::$Ring) = $Ring(a.v*b.v)
+         ==(a::$Ring, b::$Ring) = (a-b).v == 0
 
          return $Ring(X)
        end)
