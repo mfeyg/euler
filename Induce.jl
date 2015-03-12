bad95bb1b24858fc1f6a7bc4cc7de39e7738db61
Mendel Feygelson <mfeygelson@gmail.com>
2015-03-11 21:53:28 -0400

    parsing ASTs is kind of silly

--- Induce.jl
+++ Induce.jl
@@ -1,5 +1,5 @@
 module Induce
-export induce
+export induce, @induce
 
 function induce{T}(::Type{T}, f, start=1)
   A = Array{T}(1024)
@@ -15,5 +15,31 @@ function induce{T}(::Type{T}, f, start=1)
 end
 
 induce(f, init=1) = induce(Int, f, init)
+
+macro induce(T, fun)
+  @assert isa(fun,Expr)
+  @assert fun.head == :(=)
+  @assert length(fun.args) == 2
+  sig = fun.args[1]
+  def = fun.args[2]
+  @assert isa(sig,Expr)
+  @assert sig.head == :call
+  @assert length(sig.args) == 2
+  name = sig.args[1]
+  @assert isa(name, Symbol)
+  arg = sig.args[2]
+  @assert isa(arg,Expr)
+  @assert arg.head == :comparison
+  @assert length(arg.args) == 3
+  @assert arg.args[2] == :>=
+  var = arg.args[1]
+  @assert isa(var, Symbol)
+  start = arg.args[3]
+
+  return (name, T, var, start, def)
+
+  A = Array{T}(1024)
+  grow() = A = [A;A]
+end
 
 end
