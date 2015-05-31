16cb977b69b9fbf6a31cfa3d743ccf1b24938342
Mendel Feygelson <mfeygelson@gmail.com>
2015-05-31 01:25:13 -0500

    define board rules in one go

--- monopoly-odds.jl
+++ monopoly-odds.jl
@@ -20,33 +20,41 @@ Dice = sum([ Int64[ a!=b && to == mod(from + a+b, B) ||
 board = 0:B-1
 
 # these are the squares with special rules:
-g2j=[30]
-cc=[2,17,33]
-ch=[7,22,36]
+g2j=[30]       # go to jail
+cc=[2,17,33]   # communtiy chest
+ch=[7,22,36]   # chance
 
 # to find the next railroad and utility:
 nextRR(b) = 5 + 10 * mod(div(b+5,10), 4)
 nextUtil(b) = 12 <= b < 28 ? 28 : 12
 
-# Now we can define the board rules
-G2J = Int64[ in(from, g2j) && to==jail for to=board, from=board ]
-CC = Int64[ in(from,cc) * (14*(to==from) + (to==0 || to==jail)) for to=board, from=board] / 16
-CH = Int64[ in(from,ch) * (in(to, [0,10,11,24,39,5]) + 2 * (to == nextRR(from))
-                                                     + (to == nextUtil(from))
-                                                     + (to == from - 3)
-                                                     + 6 * (to == from)
-                           ) for to=board, from=board ] / 16
+# Now we can define the board rules.
 
-Board = G2J + CC + CH + Int64[to==from && !in(from, [g2j;cc;ch]) for to=board, from=board]
-
-# since our board rules ignore doubles, just repeat them along the diagonal
+# since our board rules ignore doubles, they just repeat along the diagonal:
 diagonal(M) =
   let O = zero(M)
     [M O O;
      O M O;
      O O M]
   end
-Board = diagonal(Board)
+
+Board = diagonal(Float64[
+  if in(from, g2j)
+    to == jail
+  elseif in(from, cc)
+    (14*(to==from) + (to==0) + (to==jail)) / 16
+  elseif in(from, ch)
+    (sum(to .== [0,10,11,24,39,5])
+     + 2 * (to == nextRR(from))
+     + (to == nextUtil(from))
+     + (to == from - 3)
+     + 6 * (to == from)
+     ) / 16
+  else
+    to == from
+  end
+
+  for to=board, from=board ])
 
 # the full results of a single move: first roll the dice, then follow the rules
 Move = Board * Dice
