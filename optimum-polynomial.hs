002b1f4b65744f74e565ebba3952ce21ff612f69
Mendel Feygelson <mfeygelson@gmail.com>
2015-07-13 21:46:59 -0500

    solve 101

---
+++ optimum-polynomial.hs
@@ -0,0 +1,17 @@
+import Data.Ratio
+
+u n = sum [(-n)^i | i <- [0..10]]
+
+approx k f = lagrange [(x, f x) | x <- [1..k]]
+
+-- Each term of a sequence paired with the rest of the sequence
+-- parts [x i | i <- [1..k]] = [(x i, [x j | j <- [1..k], j /= i]) | i <- [1..k]]
+parts = go id
+  where go xs (y:ys) = (y, xs ys) : go (xs . (y:)) ys
+        go _ [] = []
+
+lagrange pts x = sum $ map (`term` x) (parts pts)
+
+term ((xj,yj), rest) x = (yj%1) * product [(x-xm)%(xj-xm) | (xm,_) <- rest]
+
+soln = sum [approx i u (i+1) | i <- [1..10]]
